bd1184b8ce56155d69068bc11fc469a5a218f753
arsalanwahid <arslanwahid1993@gmail.com>
2021-01-06 22:39:07 +0500

    Initial commit

---
+++ Cows and Bull/Cows and Bull/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Cows and Bull
+//
+//  Created by Arsalan Wahid Asghar on 06/01/2021.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
